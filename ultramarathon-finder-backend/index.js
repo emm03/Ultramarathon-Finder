9d6c2854c3067e0f44e93df64d5adfe038740b35
emm03 <emmibishop3@gmail.com>
2025-06-09 14:00:20 -0700

    Fix CORS: allow PATCH method from frontend

--- ultramarathon-finder-backend/index.js
+++ ultramarathon-finder-backend/index.js
@@ -41,7 +41,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Session-Id'],
 }));
 
