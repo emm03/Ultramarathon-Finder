a0e10932519186fb7e77035c2297bb6a5442064e
emm03 <emmibishop3@gmail.com>
2025-07-01 16:58:44 -0700

    Switch to Mapbox polyline library for Strava map decoding

--- frontend/ultra_map_script.js
+++ frontend/ultra_map_script.js
@@ -288,7 +288,7 @@ function openUltraModal(race) {
     polylineMap.innerHTML = ""; 
 
     if (race.map && race.map.summary_polyline) {
-        const decoded = L.PolylineUtil.decode(race.map.summary_polyline);
+        const decoded = polyline.decode(race.map.summary_polyline);
 
         const modalMap = L.map("polyline-map", {
             scrollWheelZoom: false,
